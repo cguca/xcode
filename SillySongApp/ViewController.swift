5f22561c7aa80be0d327b3f2d7257a11239d726f
Cary Guca <cary.guca@gmail.com>
2020-05-16 09:27:37 -0500

    Initial Commit

---
+++ SillySongApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SillySongApp
+//
+//  Created by Cary Guca on 5/16/20.
+//  Copyright © 2020 Cary Guca. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
