ff086a0f88731999c6319b835687f2a55a875d37
Cary Guca <cary.guca@gmail.com>
2020-05-15 19:00:33 -0500

    Initial Commit

---
+++ Silly Song/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Silly Song
+//
+//  Created by Cary Guca on 5/15/20.
+//  Copyright © 2020 Cary Guca. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
